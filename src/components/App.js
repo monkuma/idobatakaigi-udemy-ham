91284d60aa05979db72dc18ea785791f440f067d
monkuma <kumamoto.jobs@gmail.com>
2022-10-25 20:13:53 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { useState } from "react";
 import Main from "./Main";
 import SignIn from "./Signin";
+import config from "../config.json";
 
 export default () => {
   const [name, setName] = useState("");
   console.log({ name }, "in App.js");
 
-  if (name === "") {
+  if (config.signInEnabled && name === "") {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
