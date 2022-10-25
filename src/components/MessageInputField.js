610147f97c1378110aba07a3e6be2373417f85f2
monkuma <kumamoto.jobs@gmail.com>
2022-10-25 21:46:30 +0900

    update layout

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import { Avatar, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: "26px",
   },
 });
 const MessageInputField = () => {
   const calsses = useStyles();
-  return <div className={calsses.root}>MessageInputField</div>;
+  return (
+    <div className={calsses.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>入力してください</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default MessageInputField;
